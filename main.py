68313030fa64ddbdf3540d650c69c9fda22ce0d9
AgataJuda <272509@student.pwr.edu.pl>
2023-11-20 22:41:52 +0100

    zmiany

--- main.py
+++ main.py
@@ -3,48 +3,67 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+from scipy.signal import convolve
 
-def f1(x):
+
+def simplesin(x):
     return np.sin(x)
 
-def f2(x):
-    return np.sin(x - 1)
+def inverted_sin(x):
+    return np.sin(np.power(x, -1))
 
-def f3(x):
+def sign(x):
     return np.sign(np.sin(8 * x))
 
-def function(x, function_id):
-    if function_id == 1:
-        return f1(x)
-    elif function_id == 2:
-        return f2(x)
-    elif function_id == 3:
-        return f3(x)
-    else:
-        raise ValueError("Niepoprawny identyfikator funkcji")
+def interpolate_function(x, kernel):
+    return convolve(x, kernel, mode='same', method='direct') / np.sum(kernel)
 
-x = np.linspace(-np.pi, np.pi, 100)
+kernels = {
+    'boxcar': np.ones(3)/3,
+    'hamming': np.hamming(3),
+    'gaussian': np.array([1, 2, 1])/3,
+    'sobel': np.array([-1, 0, 1]),
+    'laplacian': np.array([1, -2, 1])
+}
 
-N = len(x)
+N = 100
+multipliers = [2, 4, 10]
 
-kernels = [np.ones(3), np.array([1, 2, 1]), np.array([1, 0, -1]), np.array([-1, 0, 1]), np.array([-1, 2, -1])]
+x_original = np.linspace(-np.pi, np.pi, N)
+y_original = simplesin(x_original)
 
-point_counts = [N, 2 * N, 4 * N, 10 * N]
+plt.plot(x_original, y_original, label='Original function', color='blue')
 
-for function_id in [1, 2, 3]:
-    print(f"Function: {function.__name__}")
-    for kernel in kernels:
-        print(f"Kernel: {kernel}")
-        for point_count in point_counts:
-            x_interpolated = np.linspace(-np.pi, np.pi, point_count)
-            y_original = function(x_interpolated, function_id)
-            y_interpolated = np.convolve(y_original, kernel, mode='same')
-            kernel_sum = np.sum(kernel)
-            if kernel_sum != 0:
-                y_interpolated /= kernel_sum
-            mse = np.mean((y_original - y_interpolated) ** 2)
-            print(f"Function: {function_id}, Point count: {point_count}, MSE: {mse}")
+inverted_y = inverted_sin(x_original)
+sign_y = sign(x_original)
 
 
+for kernel_name, kernel in kernels.items():
+    for multiplier in multipliers:
+
+        x_interpolate = np.linspace(-np.pi, np.pi, N * multiplier)
+        y_interpolate = interpolate_function(y_original, kernel)[:len(x_interpolate)]
+
+        mse_original = np.mean((simplesin(x_interpolate) - y_interpolate)**2)
+        print(f'MSE for {kernel_name} kernel with {multiplier} times more points (original function): {mse_original}')
+
+        plt.plot(x_interpolate, y_interpolate, label=f'{kernel_name} kernel, {multiplier}x points (original function)', alpha=0.7)
+
+        y_interpolate_inverted = interpolate_function(inverted_y, kernel)[:len(x_interpolate)]
+
+        mse_inverted = np.mean((inverted_sin(x_interpolate) - y_interpolate_inverted)**2)
+        print(f'MSE for {kernel_name} kernel with {multiplier} times more points (inverted_sin): {mse_inverted}')
+
+        plt.plot(x_interpolate, y_interpolate_inverted, label=f'{kernel_name} kernel, {multiplier}x points (inverted_sin)', alpha=0.7)
+
+        y_interpolate_sign = interpolate_function(sign_y, kernel)[:len(x_interpolate)]
+
+        mse_sign = np.mean((sign(x_interpolate) - y_interpolate_sign)**2)
+        print(f'MSE for {kernel_name} kernel with {multiplier} times more points (sign): {mse_sign}')
+
+        plt.plot(x_interpolate, y_interpolate_sign, label=f'{kernel_name} kernel, {multiplier}x points (sign)', alpha=0.7)
+
+plt.legend()
+plt.show()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
